ee4a4ad1ebe8277d682bf6ac9c1f67491d3f992a
shines77 <gz_shines@msn.com>
2019-01-08 23:06:22 +0800

    MainForm增加了聊天消息和礼物信息的显示(使用RichTextBox控件);
    Signed-off-by: shines77 <gz_shines@msn.com>

--- HuyaLiveHelper/MainForm.Designer.cs
+++ HuyaLiveHelper/MainForm.Designer.cs
@@ -28,23 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.cbBoxRoomId = new System.Windows.Forms.ComboBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblRoomId = new System.Windows.Forms.Label();
+            this.txtBoxRoomId = new System.Windows.Forms.TextBox();
+            this.chatContent = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // cbBoxRoomId
+            // 
+            this.cbBoxRoomId.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbBoxRoomId.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.cbBoxRoomId.FormattingEnabled = true;
+            this.cbBoxRoomId.Location = new System.Drawing.Point(217, 11);
+            this.cbBoxRoomId.Name = "cbBoxRoomId";
+            this.cbBoxRoomId.Size = new System.Drawing.Size(137, 22);
+            this.cbBoxRoomId.TabIndex = 10;
+            this.cbBoxRoomId.SelectedIndexChanged += new System.EventHandler(this.cbBoxRoomId_SelectedIndexChanged);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnLogin.Location = new System.Drawing.Point(362, 10);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 9;
+            this.btnLogin.Text = "登录";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lblRoomId
+            // 
+            this.lblRoomId.AutoSize = true;
+            this.lblRoomId.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblRoomId.Location = new System.Drawing.Point(12, 16);
+            this.lblRoomId.Name = "lblRoomId";
+            this.lblRoomId.Size = new System.Drawing.Size(91, 14);
+            this.lblRoomId.TabIndex = 8;
+            this.lblRoomId.Text = "虎牙房间号：";
+            // 
+            // txtBoxRoomId
+            // 
+            this.txtBoxRoomId.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtBoxRoomId.Location = new System.Drawing.Point(107, 11);
+            this.txtBoxRoomId.Name = "txtBoxRoomId";
+            this.txtBoxRoomId.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxRoomId.TabIndex = 7;
+            // 
+            // chatContent
+            // 
+            this.chatContent.AcceptsTab = true;
+            this.chatContent.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.chatContent.Location = new System.Drawing.Point(12, 45);
+            this.chatContent.Name = "chatContent";
+            this.chatContent.ReadOnly = true;
+            this.chatContent.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.chatContent.Size = new System.Drawing.Size(656, 544);
+            this.chatContent.TabIndex = 6;
+            this.chatContent.Text = "";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(641, 502);
+            this.ClientSize = new System.Drawing.Size(679, 605);
+            this.Controls.Add(this.cbBoxRoomId);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblRoomId);
+            this.Controls.Add(this.txtBoxRoomId);
+            this.Controls.Add(this.chatContent);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "HuyaChatHelper";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cbBoxRoomId;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblRoomId;
+        private System.Windows.Forms.TextBox txtBoxRoomId;
+        private System.Windows.Forms.RichTextBox chatContent;
     }
 }
 
--- HuyaLiveHelper/MainForm.cs
+++ HuyaLiveHelper/MainForm.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿
+using System;
 using System.Diagnostics;
 using System.Windows.Forms;
+using System.Drawing;
 
 using HuyaLive;
 
@@ -14,18 +16,48 @@ using HuyaLive;
 
 namespace HuyaLiveHelper
 {
+    public struct RoomIdInfo
+    {
+        public string roomId;
+        public string roomIntro;
+    }
+
     public partial class MainForm : Form, ClientListener
     {
         private HuyaLiveClient client = null;
 #if DEBUG
         private Logger logger = new Logger(new HuyaLive.Debugger());
 #else
-        private Logger logger = new Logger(new HuyaLive.Consoler());
+        private Logger logger = new Logger(new HuyaLive.DummyLogger());
 #endif
+
+        private AtomicInt msg_cnt = new AtomicInt();
+        private AtomicInt closing = new AtomicInt();
+
+        private bool isActived = false;
+        private string curRoomId = "";
+
+        private static RoomIdInfo[] roomIdInfos = {
+            new RoomIdInfo { roomId = "0",        roomIntro = "请选择一个主播" },
+            new RoomIdInfo { roomId = "18001",    roomIntro = "18001 (洋气黄)" },
+            new RoomIdInfo { roomId = "399910",   roomIntro = "399910 (神秘狗)" },
+            new RoomIdInfo { roomId = "520880",   roomIntro = "520880 (拉风龙)" },
+            new RoomIdInfo { roomId = "521000",   roomIntro = "521000 (卡尔)" },
+            new RoomIdInfo { roomId = "626813",   roomIntro = "626813 (女王盐)" },
+            new RoomIdInfo { roomId = "666007",   roomIntro = "666007 (大申屠)" },
+            new RoomIdInfo { roomId = "908400",   roomIntro = "908400 (董小飒)" },
+            new RoomIdInfo { roomId = "931827",   roomIntro = "931827 (大圣归来)" },
+            new RoomIdInfo { roomId = "15382773", roomIntro = "15382773 (郭子)" },
+        };
+
+        private delegate void delegateChatMsg(string nickname, string content);
+        private delegate void delegateGiftMsg(string nickname, string itemName, int itemCount);
+
         public MainForm()
         {
             InitializeComponent();
             CheckForIllegalCrossThreadCalls = false;
+            closing.Set(0);
         }
 
         public void OnClientStart(object sender)
@@ -58,6 +90,8 @@ namespace HuyaLiveHelper
                               message.uid, message.imid, message.nickname);
             logger?.WriteLine(">>>  content = \"{0}\", length = {1}",
                               message.content, message.length);
+
+            AppendChatMsg(message.nickname, message.content);
         }
 
         public void OnUserGift(object sender, UserGiftMessage message)
@@ -68,6 +102,8 @@ namespace HuyaLiveHelper
                               message.uid, message.imid, message.nickname);
             logger?.WriteLine(">>>  itemName = \"{0}\", itemCount = {1}, itemPrice = {2}, ",
                               message.itemName, message.itemCount, message.itemPrice);
+
+            AppendGiftMsg(message.nickname, message.itemName, message.itemCount);
         }
 
         public void OnFreshGiftList(object sender, GiftListMessage message)
@@ -81,6 +117,8 @@ namespace HuyaLiveHelper
             logger?.WriteLine(">>>  timestamp = {0}", message.timestamp);
             logger?.WriteLine(">>>  roomId = {0}, onlineUsers = {1}",
                               message.roomId, message.onlineUsers);
+
+            AppendChatMsg("系统消息", "当前在线人数: " + message.onlineUsers.ToString());
         }
 
         public void OnVipEnter(object sender, VipEnterMessage message)
@@ -91,6 +129,10 @@ namespace HuyaLiveHelper
                               message.uid, message.imid, message.nickname);
             logger?.WriteLine(">>>  name = \"{0}\", level = {1}",
                               message.noblename, message.level);
+
+            string welcome = string.Format("欢迎 [{0}] [{1}] 进入直播间。",
+                                           message.noblename, message.nickname);
+            AppendChatMsg("系统消息", welcome);
         }
 
         public void OnNobleOnline(object sender, NobleOnlineMessage message)
@@ -103,8 +145,139 @@ namespace HuyaLiveHelper
                               message.noblename, message.level);
         }
 
+        public void ClearChatContent()
+        {
+            chatContent.Focus();
+            chatContent.Clear();
+            chatContent.ClearUndo();
+        }
+
+        public void AppendChatMsg(string nickname, string content)
+        {
+            if (this.Handle == null || closing.GetAndAdd(0) == 1)
+            {
+                return;
+            }
+
+            if (chatContent.InvokeRequired)
+            {
+                delegateChatMsg delegates = new delegateChatMsg(AppendChatMsg);
+                chatContent.Invoke(delegates, nickname, content);
+            }
+            else
+            {
+                int cnt = msg_cnt.Increment();
+                if (cnt > 500)
+                {
+                    chatContent.Clear();
+                    chatContent.ClearUndo();
+                    msg_cnt.Set(0);
+                    GC.Collect();
+                }
+
+                if (!isActived && (cnt % 10) == 9)
+                {
+                    chatContent.Focus();
+                    chatContent.Select(chatContent.TextLength, 0);
+                    chatContent.ScrollToCaret();
+                }
+
+                string text;
+                //text = string.Format("[{0}]: {1}\n", nickname, content);
+                //text = cnt.ToString() + "\n";
+                //chatContent.AppendText(text);
+
+                chatContent.SelectionStart = chatContent.TextLength;
+                chatContent.SelectionLength = 0;
+                chatContent.SelectionColor = Color.Black;
+                chatContent.AppendText("[");
+
+                chatContent.SelectionStart = chatContent.TextLength;
+                chatContent.SelectionLength = 0;
+                chatContent.SelectionColor = Color.Blue;
+                chatContent.AppendText(nickname);
+
+                chatContent.SelectionStart = chatContent.TextLength;
+                chatContent.SelectionLength = 0;
+                chatContent.SelectionColor = Color.Black;
+                text = string.Format("]: {0}\n", content);
+                chatContent.AppendText(text);
+
+                //chatContent.Focus();
+            }
+        }
+
+        public void AppendGiftMsg(string nickname, string itemName, int itemCount)
+        {
+            if (this.Handle == null || closing.GetAndAdd(0) == 1)
+            {
+                return;
+            }
+
+            if (chatContent.InvokeRequired)
+            {
+                delegateGiftMsg delegates = new delegateGiftMsg(AppendGiftMsg);
+                chatContent.Invoke(delegates, nickname, itemName, itemCount);
+            }
+            else
+            {
+                int cnt = msg_cnt.Increment();
+                if (cnt > 500)
+                {
+                    chatContent.Clear();
+                    chatContent.ClearUndo();
+                    msg_cnt.Set(0);
+                    GC.Collect();
+                }
+
+                //chatContent.Focus();
+
+                if (!isActived && (cnt % 10) == 9)
+                {
+                    chatContent.Focus();
+                    chatContent.Select(chatContent.TextLength, 0);
+                    chatContent.ScrollToCaret();
+                }
+
+                string text;
+                //text = string.Format("[{0}]: {1} x {2}\n",
+                //                     nickname, itemName, itemCount);
+                //text = cnt.ToString() + "\n";
+                //chatContent.AppendText(text);
+
+                chatContent.SelectionStart = chatContent.TextLength;
+                chatContent.SelectionLength = 0;
+                chatContent.SelectionColor = Color.Black;
+                chatContent.AppendText("[");
+
+                chatContent.SelectionStart = chatContent.TextLength;
+                chatContent.SelectionLength = 0;
+                chatContent.SelectionColor = Color.Blue;
+                chatContent.AppendText(nickname);
+
+                chatContent.SelectionStart = chatContent.TextLength;
+                chatContent.SelectionLength = 0;
+                chatContent.SelectionColor = Color.Black;
+                chatContent.AppendText("]: ");
+
+                chatContent.SelectionStart = chatContent.TextLength;
+                chatContent.SelectionLength = 0;
+                chatContent.SelectionColor = Color.Red;
+                chatContent.AppendText(itemName);
+
+                chatContent.SelectionStart = chatContent.TextLength;
+                chatContent.SelectionLength = 0;
+                chatContent.SelectionColor = Color.Black;
+                text = string.Format(" x {0}\n", itemCount);
+                chatContent.AppendText(text);
+
+                //chatContent.Focus();
+            }
+        }
+
         private void MainForm_Load(object sender, EventArgs e)
         {
+            /*
             try
             {
                 client = new HuyaLiveClient(this);
@@ -125,8 +298,37 @@ namespace HuyaLiveHelper
             }
             catch (Exception ex)
             {
-                logger.WriteLine("Exception: " + ex.ToString());
-                logger.WriteLine("");
+                logger?.WriteLine("Exception: " + ex.ToString());
+                logger?.WriteLine("");
+            }
+            //*/
+
+            try
+            {
+                client = new HuyaLiveClient(this);
+                //client.SetMobileMode(false);
+                client.SetLogger(logger);
+
+                isActived = true;
+
+                cbBoxRoomId.Items.Clear();
+
+                int infoLength = roomIdInfos.Length;
+                ListItem[] listItems = new ListItem[infoLength];
+                for (int i = 0; i < infoLength; i++)
+                {
+                    RoomIdInfo info = roomIdInfos[i];
+                    listItems[i] = new ListItem(info.roomId, info.roomIntro);
+                    cbBoxRoomId.Items.Add(listItems[i]);
+                }
+
+                cbBoxRoomId.SelectedIndex = 0;
+                cbBoxRoomId.SelectedItem = listItems[0];
+            }
+            catch (Exception ex)
+            {
+                logger?.WriteLine("Exception: " + ex.ToString());
+                logger?.WriteLine("");
             }
         }
 
@@ -143,6 +345,68 @@ namespace HuyaLiveHelper
                 client.SetLogger(null);
                 client.SetListener(null);
                 client = null;
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (client == null)
+                {
+                    client = new HuyaLiveClient(this);
+                    //client.SetMobileMode(false);
+                    client.SetLogger(logger);
+                }
+
+                if (client != null)
+                {
+                    string roomId = txtBoxRoomId.Text;
+                    roomId.Trim();
+
+                    if (roomId == "")
+                    {
+                        MessageBox.Show("房间号不能为空!", "HuyaHelper");
+                        return;
+                    }
+
+                    if (roomId == curRoomId && roomId != "")
+                    {
+                        // Current roomId is not change, directly return.
+                        return;
+                    }
+
+                    if (client.IsRunning())
+                    {
+                        client.Stop();
+                    }
+
+                    ClearChatContent();
+
+                    client.Start(roomId);
+
+                    // Record connected roomId.
+                    curRoomId = roomId;
+                }
+            }
+            catch (Exception ex)
+            {
+                logger?.WriteLine("Exception: " + ex.ToString());
+                logger?.WriteLine("");
+            }
+        }
+
+        private void cbBoxRoomId_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListItem listItem = cbBoxRoomId.SelectedItem as ListItem;
+            if (listItem != null)
+            {
+                string roomId = listItem.Id;
+                roomId.Trim();
+                if (roomId != "" && roomId != "0")
+                {
+                    txtBoxRoomId.Text = roomId;
+                }
             }
         }
     }
